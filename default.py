e17bfa81d8ebbb05b0b2dd78953e06b39cbaf681
Andre LeBlanc <andrepleblanc@gmail.com>
2011-03-19 16:35:13 -0400

    0.1.11

--- default.py
+++ default.py
@@ -13,7 +13,7 @@ __plugin__ = "Canada On Demand"
 __author__ = 'Andre,Renaud  {andrepleblanc,renaudtrudel}@gmail.com'
 __url__ = 'http://github.com/andrepl/plugin.video.canada.on.demand/'
 __date__ = '03-19-2011'
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 __settings__ = xbmcaddon.Addon(id='plugin.video.canada.on.demand')
 
 
