0c51f84762341704865bcb4a2b3de1b43c2ee5ed
Liam R. Howlett <howlett@gmail.com>
2011-11-08 19:27:41 -0500

    Pull jbrnd's fix from forum.

--- channels/canwest.py
+++ channels/canwest.py
@@ -170,7 +170,7 @@ class Showcase(CanwestBaseChannel):
     short_name = 'showcase'
     long_name = 'Showcase'
     PID = 'sx9rVurvXUY4nOXBoB2_AdD1BionOoPy'
-    #swf_url = 'http://www.showcase.ca/video/swf/flvPlayer.swf'
+    swf_url = 'http://www.showcase.ca/video/swf/flvPlayer.swf  swfvfy=true'
     root_depth = 2
     def get_categories_json(self,arg):
         url = CanwestBaseChannel.get_categories_json(self,arg) + '&query=CustomText|PlayerTag|z/Showcase%20Video%20Centre' #urlencode
